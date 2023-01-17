4d1701be7c411d36522768c6507c3db0ba23f38b
Mike Liang <liang.mike.to@gmail.com>
2023-01-17 10:53:33 -0500

    split into components

---
+++ src/components/ColCards.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card-component py-4">
+    <div class="border p-8 rounded-xl shadow">
+      <div class="flex flex-col">
+        <h1
+          class="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 to-pink-500"
+        >
+          {{ title }}
+        </h1>
+        <p class="lg:text-xl py-2 pb-4">{{ description }}</p>
+        <button
+          class="rounded-lg p-2 text-white font-bold sm:w-1/4 md:w-1/3 lg:w-1/2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
+          v-if="link"
+        >
+          Read More
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: false,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/ImageCards.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card-component py-4">
+    <div class="border p-8 rounded-xl shadow">
+      <div class="flex flex-col">
+        <div class="max-h-1/2">
+          <img :src="image" class="rounded-xl w-96 object-cover" />
+        </div>
+        <h1
+          class="pt-4 text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 to-pink-500"
+        >
+          {{ title }}
+        </h1>
+        <p class="text-xl py-2 pb-4">{{ description }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
