8de4b42f9202cb1d8175563446a9f1c5baef4d02
Mike Liang <liang.mike.to@gmail.com>
2023-01-24 13:52:04 -0500

    set up routing with firebase auth

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { getCurrentUser } from 'vuefire'
 import routes from './routes'
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 })
+
+router.beforeEach(async (to, from, next) => {
+  if (to.matched.some((route) => route.meta.authRequired)) {
+    const currentUser = await getCurrentUser();
+    if (currentUser) {
+      next();
+    } else {
+      next({
+        path: "/login",
+      });
+    }
+  } else {
+    next();
+  }
+});
+
 
 
 export default router;
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,4 +1,6 @@
 import Home from '@/views/Home.vue';
+import Dashboard from '@/views/Dashboard.vue';
+import NotFound from '@/views/NotFound.vue';
 
 const routes = [
   {
@@ -6,6 +8,29 @@ const routes = [
     name: 'Home',
     component: Home,
   },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard,
+    meta: {
+      authRequired: true,
+    }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/views/Login.vue'),
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import('@/views/Register.vue'),
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: NotFound,
+  }
 ]
 
 export default routes;
