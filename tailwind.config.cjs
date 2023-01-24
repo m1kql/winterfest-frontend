2ac2f0391a7bff09de3c44c4d143d5591d892adc
Mike Liang <liang.mike.to@gmail.com>
2023-01-24 13:55:07 -0500

    update deps

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,8 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     fontFamily: {
@@ -10,5 +12,7 @@ module.exports = {
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin'),
+  ],
 }
